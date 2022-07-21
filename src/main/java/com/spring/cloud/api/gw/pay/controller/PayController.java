5edc1f25ed1c37ef7d19f4ad17dd512eb70013e8
Ted <withwooyong@gmail.com>
2022-07-22 00:35:47 +0900

    aspect Logging

--- src/main/java/com/spring/cloud/api/gw/pay/controller/PayController.java
+++ src/main/java/com/spring/cloud/api/gw/pay/controller/PayController.java
@@ -1,6 +1,9 @@
 package com.spring.cloud.api.gw.pay.controller;
 
 import com.spring.cloud.api.gw.pay.service.PayService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/")
 @RequiredArgsConstructor
+@Api(tags = {"결제"})
 public class PayController {
 
     private final PayService payService;
 
+    @ApiOperation(value = "hello", response = ResponseEntity.class, notes = "")
     @GetMapping
     ResponseEntity<String> getHello() {
         String hello = payService.getHello();
@@ -26,9 +31,10 @@ public class PayController {
     /**
      * http://localhost:8092/pay?payType=kakao&price=1000
      */
+    @ApiOperation(value = "pay", response = ResponseEntity.class, notes = "")
     @RequestMapping(path = "/pay")
-    ResponseEntity<String> getPay(@RequestParam String payType, @RequestParam int price) {
-        log.debug("=============");
+    ResponseEntity<String> getPay(@ApiParam(value = "payType") @RequestParam String payType,
+                                  @ApiParam(value = "price") @RequestParam int price) {
         String result = payService.getPay(payType, price);
         return ResponseEntity.ok(result);
     }
